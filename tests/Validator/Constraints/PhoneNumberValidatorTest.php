303f96e439a7313694de5b69f8a39d9428e8c662
Olivier Dolbeau <olivier@bbnt.me>
2023-11-29 13:56:12 +0100

    Bump minimum requirements & supports SF 7.0

--- tests/Validator/Constraints/PhoneNumberValidatorTest.php
+++ tests/Validator/Constraints/PhoneNumberValidatorTest.php
@@ -21,6 +21,7 @@ use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 use Symfony\Component\Validator\Mapping\ClassMetadata;
 use Symfony\Component\Validator\Mapping\Loader\AnnotationLoader;
+use Symfony\Component\Validator\Mapping\Loader\AttributeLoader;
 use Symfony\Component\Validator\Violation\ConstraintViolationBuilderInterface;
 
 /**
@@ -68,8 +69,7 @@ class PhoneNumberValidatorTest extends TestCase
                 ->with($this->isType('string'))
                 ->willReturn($constraintViolationBuilder);
             $constraintViolationBuilder
-                ->method('addViolation')
-                ->willReturn($constraintViolationBuilder);
+                ->method('addViolation');
 
             $this->context
                 ->expects($this->once())
@@ -89,7 +89,13 @@ class PhoneNumberValidatorTest extends TestCase
     public function testValidateFromAttribute()
     {
         $classMetadata = new ClassMetadata(PhoneNumberDummy::class);
-        (new AnnotationLoader())->loadClassMetadata($classMetadata);
+        if (class_exists(AttributeLoader::class)) {
+            (new AttributeLoader())->loadClassMetadata($classMetadata);
+        } else {
+            // AnnotationLoader have been removed in favor of AttributeLoader in 7.0
+            /* @phpstan-ignore-next-line */
+            (new AnnotationLoader())->loadClassMetadata($classMetadata);
+        }
 
         [$constraint1] = $classMetadata->properties['phoneNumber1']->constraints;
         [$constraint2] = $classMetadata->properties['phoneNumber2']->constraints;
