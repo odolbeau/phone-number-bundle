067fb79819b39ed2a364d9662fae97c000827e03
Michael Babker <michael.babker@gmail.com>
2024-05-31 08:37:26 -0500

    Use root DI extension class

--- src/DependencyInjection/MisdPhoneNumberExtension.php
+++ src/DependencyInjection/MisdPhoneNumberExtension.php
@@ -15,8 +15,8 @@ namespace Misd\PhoneNumberBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * Bundle extension.
